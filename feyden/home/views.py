7c6629c095892b9dab5339308862360ed13af4f1
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2024-10-17 14:52:46 +0100

    created products  app

--- feyden/home/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    """View to return the index.html page"""
-    return render(request, 'home/index.html')
--- my_django_project/feyden/settings.py
+++ my_django_project/feyden/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-     'allauth',
+    'allauth',
     'allauth.account',
     # Optional -- requires install using `django-allauth[socialaccount]`.
     'allauth.socialaccount',
