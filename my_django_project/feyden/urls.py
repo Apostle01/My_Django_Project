b6f83083fc301b2d8f7ed6f9a842dd568d453afa
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2024-10-17 14:52:46 +0100

    created products  app

--- my_django_project/feyden/urls.py
+++ my_django_project/feyden/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Comma added here
+    path('', include('home.urls')),  # Comma added here
     path('accounts/', include('allauth.urls')),
 ]# Serve media files in development
 if settings.DEBUG:
