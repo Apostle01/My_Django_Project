261d8ef3462382f070096a3a7f8415092ce6a518
Apostle01 <148454484+Apostle01@users.noreply.github.com>
2024-10-18 14:10:07 +0100

    Added homepage content

--- my_django_project/feyden/settings.py
+++ my_django_project/feyden/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'feyden',
     'home',
-    'cloth',
-    'products',
+    # 'cloth',
+    # 'products',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +65,10 @@ ROOT_URLCONF = 'feyden.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            os.path.join(BASE_DIR / 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- my_django_project/home/urls.py
+++ my_django_project/home/urls.py
@@ -1,6 +1,22 @@
+
+from django.contrib import admin
 from django.urls import path
-from .import views
+from . import views
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
+    # path('admin/', admin.site.urls),
     path('', views.index, name='home'),
+    # path('accounts/', include('allauth.urls')),  # User authentication
+    # path('cloth/', include('cloth.urls')),  # Routes to the cloth app (for managing cloths like kente)
 ]
+
+# Serve media files in development
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
