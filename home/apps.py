f809798177fab0a53dcc32497563c26dae5c723a
Apostle01 <nanaafianyamekye2@gmail.com>
2025-11-19 18:43:22 +0000

    Added database connection test

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
+from django.db import connection
+import logging
 
+logger = logging.getLogger(__name__)
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+    def ready(self):
+        self.test_db_connection()
+
+    def test_db_connection(self):
+        try:
+            connection.ensure_connection()
+            logger.info("Database is reachable.")
+        except Exception as e:
+            logger.error(f"Unable to connect to database: {e}")
