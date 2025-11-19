7044109d173e91c5fcfd26205813eb467a3bf7af
Apostle01 <nanaafianyamekye2@gmail.com>
2025-11-19 19:49:46 +0000

    Modified Procfile and settings.py

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web: gunicorn my_django_project.wsgi:application --bind 0.0.0.0:8080 
-web: gunicorn --env DJANGO_SETTINGS_MODULE=my_django_project.settings 
-my_django_project.wsgi:application --bind 0.0.0.0:8080
+web: gunicorn my_django_project.wsgi:application
--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -35,8 +35,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["localhost",
-    "127.0.0.1", "mydjangoproject-production.up.railway.app"]
+ALLOWED_HOSTS = ["mydjangoproject-production.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = [
     "https://mydjangoproject-production.up.railway.app",
 ]
