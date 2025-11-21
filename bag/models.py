87a806efe39d5342a5febc7f671e09be64e96e89
Apostle01 <nanaafianyamekye2@gmail.com>
2025-11-21 23:27:22 +0000

    Add models for bag and home applications, create initial migrations

--- bag/models.py
+++ bag/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from products.models import Product  # Assuming you have a Product model
 
-# Create your models here.
+class Bag(models.Model):
+    
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set the creation date
+    updated_at = models.DateTimeField(auto_now=True)  # Automatically update the date on save
+
+    def __str__(self):
+        return f"{self.user.username}'s Bag"
+
+class BagItem(models.Model):
+    bag = models.ForeignKey(Bag, related_name='items', on_delete=models.CASCADE)  # Related to the Bag
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)  # Link to Product model
+    quantity = models.PositiveIntegerField(default=1)  # Number of items
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in {self.bag.user.username}'s Bag"
--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -146,7 +146,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL:
+if DATABASE_URL and DATABASE_URL.startswith("postgres"):
     # Running on Railway
     DATABASES = {
         "default": dj_database_url.config(
@@ -163,21 +163,7 @@ else:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-# if os.getenv("DATABASE_URL"):
-#     DATABASES = {
-#         "default": dj_database_url.config(
-#             default=os.getenv("DATABASE_URL"),
-#             conn_max_age=600,
-#             ssl_require=True,
-#         )
-#     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         }
-#     }
+           
 
 # DATABASES = {
 #     'default': {
